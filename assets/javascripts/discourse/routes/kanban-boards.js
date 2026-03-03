ddc0dd7261ebf1d06a651bc42e01b5d2679319b7
Jake Goldsborough <ducks@discourse.org>
2026-03-03 09:15:39 -0800

    FEATURE: adding page titles to kanban routes

--- assets/javascripts/discourse/routes/kanban-boards.js
+++ assets/javascripts/discourse/routes/kanban-boards.js
@@ -1,7 +1,12 @@
 import { ajax } from "discourse/lib/ajax";
 import DiscourseRoute from "discourse/routes/discourse";
+import { i18n } from "discourse-i18n";
 
 export default class KanbanBoardsRoute extends DiscourseRoute {
+  titleToken() {
+    return i18n("discourse_kanban.manage.title");
+  }
+
   async model() {
     const data = await ajax("/kanban/boards.json");
     return data.boards;
