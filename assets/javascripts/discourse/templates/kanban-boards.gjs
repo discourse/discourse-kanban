421df567bcd33c3c9ddb300f185414bf453314b8
Martin Brennan <martin@discourse.org>
2026-09-02 17:17:19 +1000

    DEV: Archive repo, plugin moved into core (#98)
    The Kanban plugin has been moved into Discourse core, see
    https://github.com/discourse/discourse/pull/43056
    
    This removes all files from the repository and leaves only a README
    pointing at the core PR.

--- assets/javascripts/discourse/templates/kanban-boards.gjs
+++
@@ -1,8 +0,0 @@
-import KanbanBoardsPage from "../components/kanban-boards-page";
-
-export default <template>
-  <KanbanBoardsPage
-    @boards={{@controller.model}}
-    @canManageBoards={{@controller.currentUser.can_manage_kanban_boards}}
-  />
-</template>
--- spec/fabricators/kanban_card_history_fabricator.rb
+++
@@ -1,8 +0,0 @@
-# frozen_string_literal: true
-
-Fabricator(:kanban_card_history, class_name: "DiscourseKanban::CardHistory") do
-  board { |attrs| attrs[:card]&.board || Fabricate(:kanban_board) }
-  card { |attrs| Fabricate(:kanban_card, board: attrs[:board]) }
-  acting_user { Fabricate(:user) }
-  action :card_viewed
-end
